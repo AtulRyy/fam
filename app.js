c167ad45d5f9a0e47f4fb906b22910140331747c
Atul <atulreny811@gmail.com>
2024-04-26 09:39:19 +0530

    integrated into node

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express=require('express');
 const app=express();
 
 app.set('view engine','ejs')
-app.use('/static',express.static('public'))
+app.use('/static',express.static('static'))
 
 //ROUTES
 const homeRoute=require('./routes/homeRoute')
 const cardRoute=require('./routes/createCardRoute')
+const loginRoute=require('./routes/loginRoute')
+const memberRoute=require('./routes/memberRoute')
 
 
 app.use('/',homeRoute);
-app.use('/create-card',cardRoute);
+app.use('/card',cardRoute);
+app.use('/login',loginRoute)
+app.use('/member',memberRoute)
 
 app.listen(3000,()=>{
     console.log("server is now listening on 3000");
