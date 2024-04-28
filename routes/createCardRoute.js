c712eeedc95d363c3131677e25bb27427e2315a0
Atul Reny <117465987+AtulRy@users.noreply.github.com>
2024-04-28 17:26:31 +0530

    card download added

--- routes/createCardRoute.js
+++ routes/createCardRoute.js
@@ -1,10 +1,13 @@
 const express=require('express')
 const route=express.Router();
 
+const Members=require('../models/members')
 
-route.get('/:id',(req,res)=>{
+
+route.get('/:id',async(req,res)=>{
     memberId=req.params.id
-    res.render('card',{memberId:memberId})
+    const member=await Members.findOne({_id:memberId})
+    res.render('card',{memberId:memberId,name:member.name})
 })
 route.post('/',(req,res)=>{
     
